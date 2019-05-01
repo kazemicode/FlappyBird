7e63950c851799c449d8f4ede176c59f852bc76c
Daniel Vitug <dvitug123@gmail.com>
2019-05-01 14:24:47 -0700

    Implemented Phase 1: Gravity and GameOver
    Implemented Phase 1: Gravity and GameOver

--- FlappyBird.java
+++ FlappyBird.java
@@ -8,12 +8,32 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class FlappyBird extends Actor
 {
+    double dy = 0;
+    double g = 1.3;
+    double BOOST_SPEED = -15;
     /**
      * Act - do whatever the FlappyBird wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
+        setLocation( getX(), (int)(getY() + dy )  );
+        
+        if (Greenfoot.isKeyDown("up") == true) {
+            dy = BOOST_SPEED;
+        }
+        // flappy drops out of world then game over
+        if (getY() > getWorld().getHeight()){
+            
+            
+            GameOver gameOver = gameOver GameOver(); 
+            getWorld().addObject(gameOver, getWorld().getWidth()/2, getWorld().getHeight()/2);
+            
+            Greenfoot.stop();
+        
+        
+        }
+        
+        dy = dy + g;
     }    
 }
--- FlappyWorld.java
+++ FlappyWorld.java
@@ -9,6 +9,11 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class FlappyWorld extends World
 {
 
+ 
+    
+    
+    
+    
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -16,6 +21,17 @@ public class FlappyWorld extends World
     public FlappyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 400, 1, false); 
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        FlappyBird flappyBird = new FlappyBird();
+        addObject(flappyBird,100, getHeight()/2);
     }
 }
