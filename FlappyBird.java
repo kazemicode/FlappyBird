fb664cd3c34f07150bbb6e7944b6d183bc19ac04
Austin Aceves <Austin.Aceves72@gmail.com>
2019-05-03 13:01:12 -0700

    2: Refined Flappy's movements based on velocity and created GameOver screen

--- FlappyBird.java
+++ FlappyBird.java
@@ -9,7 +9,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class FlappyBird extends Actor
 {
     double dy = 0;
-    double g = 2;
+    double g = .3;
+    double BoostSpeed = -5; 
     /**
      * Act - do whatever the Flapptbird wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -19,13 +20,25 @@ public class FlappyBird extends Actor
         // Add your action code here. 
         setLocation (getX(), (int)(getY() + dy) );
         if(Greenfoot.isKeyDown("up")==true) {
-            dy= -15; 
+            dy= BoostSpeed; 
         }
-        dy = dy + g;  
+    if(dy < 5 && dy > -5){
+    setRotation(-10);
+    }
+    
+    else if (dy < 10 && dy > -10){
+    setRotation(-40);
+    }
+    
+    else if(dy < 20 && dy > -20){
+    setRotation(100);
+    }
         
         if(this.getY()> 400){
-        System.out.println("Game Over");
+        GameOver gameOver = new GameOver(); 
+        getWorld().addObject(gameOver, getWorld().getWidth()/2, getWorld().getHeight()/2);
         Greenfoot.stop(); 
     }
-    }    
+     dy = dy + g;  
+}    
 }
