b9f5069c80ab5a4861955c21bb49247db330cf75
Joubert <jtrias@sweetwaterschools.net>
2019-05-29 10:26:53 -0700

    Implemented Phase 2: Refined Flappy's movements based on velocity and created GameOver screen
    Implemented Phase 2: Refined Flappy's movements based on velocity and created GameOver screen

--- FlappyBird.java
+++ FlappyBird.java
@@ -10,6 +10,7 @@ public class FlappyBird extends Actor
 {
     double dy = 0;
     double g = 1.3;
+    double BOOST_SPEED = -15;
     
     /**
      * Act - do whatever the FlappyBird wants to do. This method is called whenever
@@ -19,12 +20,34 @@ public class FlappyBird extends Actor
     {
         setLocation( getX(), (int)(getY() + dy));
         
-       
         // If user pressed UP arrow, launch Flappy Bird upward
         if (Greenfoot.isKeyDown("up") == true)
         {
-            dy = -15;
+            dy = BOOST_SPEED;
+        }
+        if (dy < 10 && dy > 0)
+        {
+            setRotation(30);
+        }
+        else if ( )
+        {
+            a
+        }
+        else if ( )
+        {
+            a
+        }    
+        // If FlappyBird drops out of the world, 
+        if(this.getY() > 400)
+        {
+            displayGameOver();
         }
         dy = dy + g;
+    }
+    private void displayGameOver()
+    {
+        GameOver gameOver = new GameOver();
+        getWorld().addObject(gameOver,getWorld().getWidth()/2,getWorld().getHeight()/2);
+        Greenfoot.stop();
     }    
 }
